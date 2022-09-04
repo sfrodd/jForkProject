7575b6fd361bfa5c1b3f09d4868a631ec76a748c
sfrodd <sfroddgit@git.edu>
2022-09-04 14:34:45 +0530

    initial commit

---
+++ myScripts.js
@@ -0,0 +1,43 @@
+
+function courses(){
+     document.getElementById("demo").innerHTML = "<h2>Software Development using Agile Approach. <br> We have three live projects currently from different business verticals.</h2>";
+}
+
+function placement(){
+
+     document.getElementById("demo").innerHTML = "<h2>We train students for placement preparations. <br> We have tied up with PEOL Software Systems for providing placement assistance.</h2>";
+}
+
+function projectguidance(){
+
+     document.getElementById("demo").innerHTML = "<h2>We provide excellent project guidance. <br> We train students on frameworks and help them develop projects using Industry best practices.</h2>";
+
+}
+
+function webtechtraining(){
+
+     document.getElementById("demo").innerHTML = "<h2>We provide training on popular Web Frameworks. <br> The popular frameworks include Springboot, Node, ReactJS, Django etc.</h2>";
+
+}
+
+function internships(){
+
+     document.getElementById("demo").innerHTML = "<h2>Internship @ jForkTS will provide an excellent opportunity to <br> Learn all the steps involving in software development. <br> The Students learn Agile model for software development <br>and get exposure to all the tools and technologies</h2>";
+
+}
+
+function webdesign(){
+
+     document.getElementById("demo").innerHTML = "<h2>We provide coaching on Web Development. <br> The training includes Full stack development.</h2>";
+
+}
+
+function allcourses(){
+
+     document.getElementById("demo").innerHTML = "<h2><ul><li>OOPS with Java</li><li>Python</li><li>Data Structures and Algorithms</li></ul></h2>";
+}
+
+function placementassistance(){
+
+     document.getElementById("demo").innerHTML = "<h2>We have tied up with PEOL software Systems, Bangalore for placement assistance</h2>";
+}
